bf62727da0b9301987fa0958c2e8b7af9db42d95
Allan Bazinet <allan.bazinet@gmail.com>
2026-04-04 17:28:31 -0700

    Lint

--- src/main/java/io/github/dsheirer/module/decode/tait/Tait1200GPSMessage.java
+++ src/main/java/io/github/dsheirer/module/decode/tait/Tait1200GPSMessage.java
@@ -37,90 +37,71 @@ import java.util.TimeZone;
 
 public class Tait1200GPSMessage extends Message
 {
+    private static final int[] SIZE = {20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35};
+    private static final int[] FROM_DIGIT_1 = {36, 37, 38, 39, 40, 41, 42, 43};
+    private static final int[] FROM_DIGIT_2 = {44, 45, 46, 47, 48, 49, 50, 51};
+    private static final int[] FROM_DIGIT_3 = {52, 53, 54, 55, 56, 57, 58, 59};
+    private static final int[] FROM_DIGIT_4 = {60, 61, 62, 63, 64, 65, 66, 67};
+    private static final int[] FROM_DIGIT_5 = {68, 69, 70, 71, 72, 73, 74, 75};
+    private static final int[] FROM_DIGIT_6 = {76, 77, 78, 79, 80, 81, 82, 83};
+    private static final int[] FROM_DIGIT_7 = {84, 85, 86, 87, 88, 89, 90, 91};
+    private static final int[] FROM_DIGIT_8 = {92, 93, 94, 95, 96, 97, 98, 99};
 
-    public static int[] REVS_1 = {0, 1, 2, 3};
-    public static int[] SYNC = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-    public static int[] SIZE = {20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35};
-    public static int[] FROM_DIGIT_1 = {36, 37, 38, 39, 40, 41, 42, 43};
-    public static int[] FROM_DIGIT_2 = {44, 45, 46, 47, 48, 49, 50, 51};
-    public static int[] FROM_DIGIT_3 = {52, 53, 54, 55, 56, 57, 58, 59};
-    public static int[] FROM_DIGIT_4 = {60, 61, 62, 63, 64, 65, 66, 67};
-    public static int[] FROM_DIGIT_5 = {68, 69, 70, 71, 72, 73, 74, 75};
-    public static int[] FROM_DIGIT_6 = {76, 77, 78, 79, 80, 81, 82, 83};
-    public static int[] FROM_DIGIT_7 = {84, 85, 86, 87, 88, 89, 90, 91};
-    public static int[] FROM_DIGIT_8 = {92, 93, 94, 95, 96, 97, 98, 99};
-    public static int[] CHECKSUM_1 = {100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
-        110, 111, 112, 113, 114, 115};
-
-    public static int[] REVS_2 = {116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
-        128, 129, 130, 131};
-    public static int[] SIZE_2 = {188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198,
+    private static final int[] SIZE_2 = {188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198,
         199, 200, 201, 202, 203};
-    public static int[] TO_DIGIT_1 = {204, 205, 206, 207, 208, 209, 210, 211};
-    public static int[] TO_DIGIT_2 = {212, 213, 214, 215, 216, 217, 218, 219};
-    public static int[] TO_DIGIT_3 = {220, 221, 222, 223, 224, 225, 226, 227};
-    public static int[] TO_DIGIT_4 = {228, 229, 230, 231, 232, 233, 234, 235};
-    public static int[] TO_DIGIT_5 = {236, 237, 238, 239, 240, 241, 242, 243};
-    public static int[] TO_DIGIT_6 = {244, 245, 246, 247, 248, 249, 250, 251};
-    public static int[] TO_DIGIT_7 = {252, 253, 254, 255, 256, 257, 258, 259};
-    public static int[] TO_DIGIT_8 = {260, 261, 262, 263, 264, 265, 266, 267};
-    public static int[] UNKNOWN_1 = {268, 269, 270, 271, 272, 273, 274, 275};
-    public static int[] CHECKSUM_2 = {276, 277, 278, 279, 280, 281, 282, 283, 284, 285,
-        286, 287, 288, 289, 290, 291};
-    public static int DIVIDER_1 = 292;
-    public static int[] HOUR_TENS = {293, 294, 295};
-    public static int[] HOUR_ONES = {296, 297, 298, 299};
-    public static int DIVIDER_2 = 300;
-    public static int[] MINUTES_TENS = {301, 302, 303};
-    public static int[] MINUTES_ONES = {304, 305, 306, 307};
-    public static int DIVIDER_3 = 308;
-    public static int[] SECONDS_TENS = {309, 310, 311};
-    public static int[] SECONDS_ONES = {312, 313, 314, 315};
-    public static int DIVIDER_4 = 316;
-    public static int[] LATITUDE_SIGN = {317, 318};
-    public static int DIVIDER_5 = 319;
-    public static int[] LATITUDE_DEGREES_TENS = {320, 321, 322, 323};
-    public static int[] LATITUDE_DEGREES_ONES = {324, 325, 326, 327};
-    public static int DIVIDER_6 = 328;
-    public static int[] LATITUDE_MINUTES_TENS = {329, 330, 331};
-    public static int[] LATITUDE_MINUTES_ONES = {332, 333, 334, 335};
-    public static int[] LATITUDE_SECONDS_HUND = {336, 337, 338, 339};
-    public static int[] LATITUDE_SECONDS_TENS = {340, 341, 342, 344};
-    public static int[] LATITUDE_SECONDS_ONES = {344, 345, 346, 347};
-    public static int DIVIDER_7 = 348;
-    public static int[] LONGITUDE_SIGN = {349, 350};
-    public static int LONGITUDE_DEGREES_HUNDREDS = 351;
-    public static int[] LONGITUDE_DEGREES_TENS = {352, 353, 354, 355};
-    public static int[] LONGITUDE_DEGREES_ONES = {356, 357, 358, 359};
-    public static int DIVIDER_9 = 360;
-    public static int[] LONGITUDE_MINUTES_TENS = {361, 362, 363};
-    public static int[] LONGITUDE_MINUTES_ONES = {364, 365, 366, 367};
-    public static int[] LONGITUDE_SECONDS_HUND = {368, 369, 370, 371};
-    public static int[] LONGITUDE_SECONDS_TENS = {372, 373, 374, 375};
-    public static int[] LONGITUDE_SECONDS_ONES = {376, 377, 378, 379};
-    public static int DIVIDER_10 = 380;
-    public static int[] DATE_DAY_TENS = {381, 382, 383};
-    public static int[] DATE_DAY_ONES = {384, 385, 386, 387};
+    private static final int[] TO_DIGIT_1 = {204, 205, 206, 207, 208, 209, 210, 211};
+    private static final int[] TO_DIGIT_2 = {212, 213, 214, 215, 216, 217, 218, 219};
+    private static final int[] TO_DIGIT_3 = {220, 221, 222, 223, 224, 225, 226, 227};
+    private static final int[] TO_DIGIT_4 = {228, 229, 230, 231, 232, 233, 234, 235};
+    private static final int[] TO_DIGIT_5 = {236, 237, 238, 239, 240, 241, 242, 243};
+    private static final int[] TO_DIGIT_6 = {244, 245, 246, 247, 248, 249, 250, 251};
+    private static final int[] TO_DIGIT_7 = {252, 253, 254, 255, 256, 257, 258, 259};
+    private static final int[] TO_DIGIT_8 = {260, 261, 262, 263, 264, 265, 266, 267};
+
+    private static final int[] HOUR_TENS = {293, 294, 295};
+    private static final int[] HOUR_ONES = {296, 297, 298, 299};
+    private static final int[] MINUTES_TENS = {301, 302, 303};
+    private static final int[] MINUTES_ONES = {304, 305, 306, 307};
+     private static final int[] SECONDS_TENS = {309, 310, 311};
+    private static final int[] SECONDS_ONES = {312, 313, 314, 315};
+     private static final int[] LATITUDE_SIGN = {317, 318};
+     private static final int[] LATITUDE_DEGREES_TENS = {320, 321, 322, 323};
+    private static final int[] LATITUDE_DEGREES_ONES = {324, 325, 326, 327};
+     private static final int[] LATITUDE_MINUTES_TENS = {329, 330, 331};
+    private static final int[] LATITUDE_MINUTES_ONES = {332, 333, 334, 335};
+    private static final int[] LATITUDE_SECONDS_HUND = {336, 337, 338, 339};
+    private static final int[] LATITUDE_SECONDS_TENS = {340, 341, 342, 344};
+    private static final int[] LATITUDE_SECONDS_ONES = {344, 345, 346, 347};
+     private static final int[] LONGITUDE_SIGN = {349, 350};
+    private static final int LONGITUDE_DEGREES_HUNDREDS = 351;
+    private static final int[] LONGITUDE_DEGREES_TENS = {352, 353, 354, 355};
+    private static final int[] LONGITUDE_DEGREES_ONES = {356, 357, 358, 359};
+     private static final int[] LONGITUDE_MINUTES_TENS = {361, 362, 363};
+    private static final int[] LONGITUDE_MINUTES_ONES = {364, 365, 366, 367};
+    private static final int[] LONGITUDE_SECONDS_HUND = {368, 369, 370, 371};
+    private static final int[] LONGITUDE_SECONDS_TENS = {372, 373, 374, 375};
+    private static final int[] LONGITUDE_SECONDS_ONES = {376, 377, 378, 379};
+     private static final int[] DATE_DAY_TENS = {381, 382, 383};
+    private static final int[] DATE_DAY_ONES = {384, 385, 386, 387};
 
     //TODO: not sure if this field is the month field or the speed field
-    public static int[] DATE_MONTH = {388, 389, 390, 391};
-    public static int[] SPEED_HUNDREDS = {388, 389, 390, 391};
+    private static final int[] DATE_MONTH = {388, 389, 390, 391};
+    private static final int[] SPEED_HUNDREDS = {388, 389, 390, 391};
 
-    public static int[] SPEED_TENS = {392, 393, 394, 395};
-    public static int[] SPEED_ONES = {396, 397, 398, 399};
-    public static int[] SPEED_TENTHS = {400, 401, 402, 403};
-
-    private static SimpleDateFormat mSDF = new SimpleDateFormat("yyyyMMdd HHmmss");
+    private static final int[] SPEED_TENS = {392, 393, 394, 395};
+    private static final int[] SPEED_ONES = {396, 397, 398, 399};
+    private static final int[] SPEED_TENTHS = {400, 401, 402, 403};
 
     private BinaryMessage mMessage;
-    private CRC mCRC;
     private TaitIdentifier mFromIdentifier;
     private TaitIdentifier mToIdentifier;
     private List<Identifier> mIdentifiers;
+    private final SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyyMMdd HHmmss");
 
     public Tait1200GPSMessage(BinaryMessage message)
     {
         mMessage = message;
+        mDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 
     public TaitIdentifier getFromIdentifier()
@@ -211,7 +192,7 @@ public class Tait1200GPSMessage extends Message
 
         sb.append(" SPEED:").append(getSpeed()).append("KPH");
         sb.append(" GPS TIME:");
-        sb.append(mSDF.format(new Date(getGPSTime())));
+        sb.append(mDateFormat.format(new Date(getGPSTime())));
 
         sb.append(" ");
         sb.append(mMessage.toString());
