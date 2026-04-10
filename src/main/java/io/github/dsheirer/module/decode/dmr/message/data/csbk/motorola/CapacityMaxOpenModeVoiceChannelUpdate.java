89fb03567bb4be3c3621d50adcff71543e7acffc
Allan Bazinet <allan.bazinet@gmail.com>
2026-04-10 14:03:17 -0700

    Bug fix: typo in bitmap

--- src/main/java/io/github/dsheirer/module/decode/dmr/message/data/csbk/motorola/CapacityMaxOpenModeVoiceChannelUpdate.java
+++ src/main/java/io/github/dsheirer/module/decode/dmr/message/data/csbk/motorola/CapacityMaxOpenModeVoiceChannelUpdate.java
@@ -45,7 +45,7 @@ public class CapacityMaxOpenModeVoiceChannelUpdate extends CSBKMessage implement
 {
     private static final int[] CHANNEL_NUMBER = new int[]{16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
     private static final int[] TALKGROUP_TS1 = new int[]{32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55};
-    private static final int[] TALKGROUP_TS2 = new int[]{56, 57, 58, 59, 60, 61, 63, 64, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79};
+    private static final int[] TALKGROUP_TS2 = new int[]{56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79};
     private TalkgroupIdentifier mTalkgroupTS1;
     private TalkgroupIdentifier mTalkgroupTS2;
     private DMRTier3Channel mChannelTS1;
